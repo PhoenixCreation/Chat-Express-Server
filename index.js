501ca4ca37ef454df7e11ddfdbfc091066d02878
PhoenixCreation <phs28052001@gmail.com>
2021-05-19 20:26:27 +0530

    Added supabase and postgresql and testing...

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import supabase_pkg from "@supabase/supabase-js";
 const { createClient } = supabase_pkg;
+import pg_pkg from "pg";
+const { Client } = pg_pkg;
+
+const client = new Client({
+  connectionString: process.env.POSTGRES_URL,
+});
+client.connect((error) =>
+  console.log(error ? error : "Connected postgres database")
+);
 
 const supabase = createClient(process.env.supabaseUrl, process.env.supabaseKey);
 
 (async () => {
-  console.log("self caaling function");
   const { data, error } = await supabase
     .from("userinfo")
     .select(`username,password`);
@@ -30,6 +38,19 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+
+// INFO: `alter publication supabase_realtime add table <table_name>;`
+
+app.get("/test", async (req, res) => {
+  try {
+    // always use toLowerCase at the end of the name or else things won't work
+    const dbname = `Alice_to_Bob`.toLowerCase();
+    res.json({ message: Math.random(), dbname });
+  } catch (err) {
+    console.log(err);
+    res.json({ err });
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
